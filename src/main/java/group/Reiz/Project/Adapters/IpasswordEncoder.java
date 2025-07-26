e6956aa2161203a2e328b8b5e7019bf2a47e02db
Tiago Reiz <tiagoreizzzz@gmail.com>
2025-07-26 00:53:40 -0300

    Adição do front no repositório

--- src/main/java/group/Reiz/Project/Adapters/IpasswordEncoder.java
+++
@@ -1,9 +0,0 @@
-package group.Reiz.Project.Adapters;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public interface IpasswordEncoder {
-    String encodePassword(String password);
-    boolean checkPassword(String password, String hashed);
-}
