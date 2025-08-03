bb23544bfb466f3e353557f4086248fe81ef35be
Tiago Reiz <tiagoreizzzz@gmail.com>
2025-08-02 23:15:54 -0300

    Ajustes iniciais no front para mostrar os produtos

--- frontend/src/app/components/home/home.component.ts
+++ frontend/src/app/components/home/home.component.ts
@@ -18,59 +18,12 @@ export interface Product {
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements AfterViewInit, OnDestroy,OnInit {
-  private mutationObserver!: MutationObserver;
-  private intersectionObserver!: IntersectionObserver;
-  private observedElements = new Set<Element>();
+export class HomeComponent implements OnInit {
+  
 
-
-  constructor(private el: ElementRef, private productsService : ProductsService) {}
+  constructor(private productsService : ProductsService) {}
   ngOnInit(): void {
     this.getProducts();
-  }
-  ngAfterViewInit(): void {
-    if (typeof window === 'undefined') return;
-
-    this.intersectionObserver = new IntersectionObserver((entries) => {
-      entries.forEach(entry => {
-        const el = entry.target as HTMLElement;
-        if (entry.isIntersecting) {
-          el.classList.add('fade-in');
-          this.intersectionObserver.unobserve(el);
-          this.observedElements.delete(el);
-        }
-      });
-    }, { threshold: 0.1 });
-
-    const observeAll = () => {
-      const targets = this.el.nativeElement.querySelectorAll('.observe');
-      targets.forEach((el: Element) => {
-        if (!this.observedElements.has(el)) {
-          this.intersectionObserver.observe(el);
-          this.observedElements.add(el);
-        }
-      });
-    };
-
-    // Tenta observar imediatamente
-    observeAll();
-
-    // Observa mudanças no DOM
-    this.mutationObserver = new MutationObserver(() => {
-      observeAll();
-    });
-
-    this.mutationObserver.observe(this.el.nativeElement, {
-      childList: true,
-      subtree: true,
-    });
-  }
-
-  ngOnDestroy(): void {
-    this.mutationObserver?.disconnect();
-    this.intersectionObserver?.disconnect();
-    this.observedElements.clear();
-    
   }
     products: Product[] = [];
   getProducts ()  {
